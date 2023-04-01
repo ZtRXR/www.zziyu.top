9f17083c079e90cd2e6e40be0b74e7b5f71b83e2
ZtRXR <Zengtudor@outlook.com>
2023-04-01 11:59:51 +0800

    main app

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print("由Zengtudor开发")
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Zengtudor!'
+    return "Hello world!"
 
 
 if __name__ == '__main__':
